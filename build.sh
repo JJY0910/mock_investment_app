682d2ab47e97433d4be0ae1924a0dae599810633
JJY0910 <abcdefghijjy@naver.com>
2026-01-06 12:53:05 +0900

    fix: vercel flutter web build config

---
+++ build.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+set -e
+
+# Flutter SDK 다운로드 및 설치 (최초 1회만)
+if [ ! -d "flutter" ]; then
+  echo "Downloading Flutter SDK..."
+  git clone --depth 1 --branch stable https://github.com/flutter/flutter.git
+fi
+
+export PATH=pwd/flutter/bin:$PATH
+
+flutter config --no-analytics
+flutter --version
+
+flutter pub get
+
+# Flutter Web Release Build
+flutter build web --release --web-renderer canvaskit
